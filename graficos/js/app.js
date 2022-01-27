913ef076320f168e7234615dd6153f43d3291f3d
2DAW04 <2DAW04@esvirgua.es>
2022-01-27 08:31:09 +0100

    Graficos

---
+++ graficos/js/app.js
@@ -0,0 +1,99 @@
+/**
+ * Autor: Sergio Matamoros Delgado
+ */
+
+'use strict';
+
+//Nombre provincia
+//let labels = [];
+
+let datos = [];
+
+
+let nombreProvincia = [];
+
+let barsColors = [];
+
+//cargar JSON
+fetch("datos/histograma.json")
+.then(respuesta => respuesta.json())
+.then(e =>  {
+    for(let indexes of e) {
+        //console.log(indexes);
+
+        //labels.push(indexes.Nombre);
+        nombreProvincia.push(indexes.MetaData[2].Nombre)
+        datos.push(indexes.Data[0].Valor)
+        barsColors.push(getRandomColor());
+
+    }
+    crearChart();
+
+});
+
+
+//console.log(labels);
+console.log(nombreProvincia);
+console.log(datos);
+
+//En el JSON solo se muestran las ciudades que tienen un código, si no tiene código
+//no las utilizaremos.
+
+function crearChart() {
+
+    //Barras
+    new Chart("bar-chart", {
+        type: "bar",//pie
+        data: {
+            labels: nombreProvincia, //nombre provincias
+            datasets: [{
+                label: "Población",
+                backgroundColor: barsColors,
+                data: datos
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Tasa de paro"
+            }
+        }
+    });
+
+    new Chart("pie-chart", {
+        type: "pie",//pie
+        data: {
+            labels: nombreProvincia, //nombre provincias
+            datasets: [{
+                label: "Población",
+                backgroundColor: barsColors,
+                data: datos
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Tasa de paro"
+            }
+        }
+    });
+    
+}
+
+/**
+ * Devuelve un color aleatoriamente
+ * @returns hex color
+ */
+function getRandomColor(moreColors = false) {
+    let letters = '0123456789ABCDEF';
+    let rndColors = ["47E5BC", "B23A48", "B23A48", "78A1BB", "283044", "F06449"]
+    let color = '#';
+    if(moreColors) {
+        for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 13)];
+        }
+    } else {
+        color += rndColors[Math.floor(Math.random()*rndColors.length)]
+    }
+    return color;
+}
--- js/app.js
+++ js/app.js
@@ -14,7 +14,3 @@ function iniciar() {
         }
     };
 }
-
-
-When $a \ne 0$, there are two solutions to \(ax^2 + bx + c = 0\) and they are
-$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$
